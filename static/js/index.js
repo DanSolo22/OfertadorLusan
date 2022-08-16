beb61e56e8939d29881dc217074cf1281f38dc66
DanSolo <danielussky@gmail.com>
2022-08-16 12:34:24 +0200

    Mejoras de la pagina web

--- static/js/index.js
+++ static/js/index.js
@@ -8,19 +8,31 @@ document.getElementById('close').onmousedown = function (e) {
     return false;
 };
 
-document.getElementById('4').onclick = function (e) {
+/*document.getElementById('4').onclick = function (e) {
     e.preventDefault();
     if (submenu) {
-        document.getElementById('4').style.marginTop = '1.4vh';
+        //document.getElementById('4').style.marginTop = '1.4vh';
         document.getElementById('drop').style.display = 'none';
         submenu = false;
     }
     else {
-        document.getElementById('4').style.marginTop = '6.83vh';
+        //document.getElementById('4').style.marginTop = '6.83vh';
         document.getElementById('drop').style.display = 'block';
         submenu = true;
     }
     return false;
+}*/
+
+document.getElementById('4').onmouseover = function (e) {
+    e.preventDefault();
+    document.getElementById('4').style.marginTop = '11.3vh';
+    document.getElementById('drop').style.display = 'block';
+}
+
+document.getElementById('4').onmouseout = function (e) {
+    e.preventDefault();
+    document.getElementById('4').style.marginTop = '0';
+    document.getElementById('drop').style.display = 'none';
 }
 
 function redirect(url) {
